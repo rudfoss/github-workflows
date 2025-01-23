2a679e4288921d5d50145be0974da62708687b9a
Thomas Haugland Rudfoss <thru@riksrevisjonen.no>
2025-01-23 13:04:07 +0100

    Test

---
+++ actions/setEnv/index.js
@@ -0,0 +1,36 @@
+const os = require("node:os")
+const fs = require("node:fs")
+const core = require("@actions/core");
+const exec = require("@actions/exec")
+const dotenv = require("dotenv")
+
+const readAndParseEnv = async (envFilePath) => {
+  if (!fs.existsSync(envFilePath)) throw new Error(`Env file ${envFilePath} does not exist`)
+  const envFile = fs.readFileSync(envFilePath, "utf8")
+  return dotenv.parse(envFile)
+}
+
+try {
+  const envFile = core.getInput("envFile")
+  const replace = core.getInput("replace") === "true"
+  const secret = core.getInput("secret") === "true"
+  
+  console.log(`::group::Load env: ${envFile}${os.EOL}`)
+  console.log("settings", {envFile, replace, secret})
+
+  const env = readAndParseEnv(envFile)
+  for (const [name, value] of Object.entries(env)) {
+    const exists = !!process.env[name]
+    if (exists && !replace) {
+      console.log(`SKIP: ${name} (exists and !replace)`)
+      continue
+    }
+
+    console.log(`${exists ? "REPLACE" : "SET"}: ${name}`)
+    await exec.exec(`echo "${name}=${value}" >> $GITHUB_ENV`)
+  }
+} catch (error) {
+  core.setFailed(error.message)
+} finally {
+  console.log(`${os.EOL}::endgroup::${os.EOL}`)
+}
